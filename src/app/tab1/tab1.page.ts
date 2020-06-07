44f6d10397b263760c9ea9651fa8b1b50df1530f
Sami Ullah <sami.ullah@cadt-solutions.com>
2020-06-07 19:00:47 +0400

    basic map added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as L from "leaflet";
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  map: L.Map
+
   constructor() {}
+
+  ngOnInit() {
+    this.map = L.map('map', {
+      center: [ 25.3791924,55.4765436 ],
+      zoom: 15,
+      renderer: L.canvas()
+    })
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // maxZoom: 12,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map)
+
+
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0);
+  }
 
 }
