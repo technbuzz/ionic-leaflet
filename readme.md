04947dd6c87697deeb9b4e2a3da529f3c2d225be
technbuzz <samiullah.khan.1989@gmail.com>
2020-06-28 12:13:06 +0400

    final commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+## About
+This repository contains the source code for Leaflet Youtube Channel. It has following branches
+
+1. Leaflet
+2. Leaflet Marker
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
 import * as L from "leaflet";
+import 'leaflet.BounceMarker'
+
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/images/marker-icon-2x.png";
 
 @Component({
   selector: 'app-tab1',
@@ -21,14 +26,36 @@ export class Tab1Page {
     })
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      // maxZoom: 12,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(this.map)
 
-
+    this.addHomeMarker();
+    
     setTimeout(() => {
       this.map.invalidateSize();
     }, 0);
   }
+  
+  addHomeMarker() {
+    const homeMarker = L.marker({ lat: 25.3791924, lng: 55.4765436 }, { bounceOnAdd: true });
+    homeMarker.addTo(this.map);
+    homeMarker.bindPopup('This is our Home marker', {
+      closeButton: true
+    });
 
+    L.circle({lat: 25.3791924, lng: 55.4765436}, {
+      color: 'steelblue',
+      radius: 500,
+      fillColor: 'steelblue',
+      opacity: 0.5
+    }).addTo(this.map)
+  }
+
+  addMarker() {
+    L.marker({lat: 25.3791924, lng: 55.4765436}, {
+      draggable: true, 
+      //@ts-ignore
+      bounceOnAdd: true
+    }).addTo(this.map)
+  }
 }
